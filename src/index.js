5ba1fac57885f6e589dd67487affe7a6b41c41e0
len-mendonca <lenmendonca2003@gmail.com>
2025-08-29 14:30:04 +0530

    check: ci/cd

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // Example route
 app.get("/", (req, res) => {
-  res.send("Hello from Express!");
+  res.send("Hello from Express deplyed from Azure DevOps!");
 });
 
 // Start the server
